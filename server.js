490bbbadf2183bb1aa36d07d70acf022fc2fdff5
David Naimi <d88naimi@gmail.com>
2017-05-30 13:57:09 -0700

    server.js made

---
+++ server.js
@@ -0,0 +1,95 @@
+
+// dependencies 
+var express = require ("express");
+var bodyParser = require(" body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var Article = require("./models/Articles.js");
+
+var app = express();
+var PORT = process.env || 8080;
+
+// Run Morgan for Logging
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:"application/vnd.api+json"}));
+
+app.use(express.static("./public"));
+
+mongoose.connect('mongodb://localhost/nytreact');
+
+// mongoose.connect('mongodb://heroku');
+
+var db = mongoose.connection;
+
+db.on("error", function(err){
+console.log("Mongoosey error: ", err);
+});
+
+db.once("open", function(){
+console.log("Mongoose connection success my friend "  );
+
+});
+
+app.get("/", function(req, res){
+res.sendFile("./public/index.html");
+
+})
+
+app.get("./api/saved", function(req, res){
+	
+	Article.find({})
+	.exec(function(err, doc){
+		if(err){
+			console.log(err);
+
+		}
+		else {
+			res.semd(doc);
+		}
+	})
+});
+
+app.post('/api/saved', function(req, res){
+
+  var newArticle = new Article({
+    title: req.body.title,
+    date: req.body.date,
+    url: req.body.url
+  });
+
+  newArticle.save(function(err, doc){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      res.json(doc);
+    }
+  });
+
+});
+
+app.delete('/api/saved/:id', function(req, res){
+
+  Article.find({'_id': req.params.id}).remove()
+    .exec(function(err, doc) {
+      res.send(doc);
+  });
+
+});
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT 8080 Davey: " + PORT);
+});
+
+
+
+
+
+
+
+
